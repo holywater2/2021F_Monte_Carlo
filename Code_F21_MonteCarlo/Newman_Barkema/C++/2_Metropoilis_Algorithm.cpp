f5c10f0810bd4622593310f2cf2f0d238db045f6
holywater2 <holywater@gist.ac.kr>
2022-01-04 12:27:46 +0900

    Metropolis Cpp Modified (complete version)

--- Code_F21_MonteCarlo/Newman_Barkema/C++/2_Metropoilis_Algorithm.cpp
+++ Code_F21_MonteCarlo/Newman_Barkema/C++/2_Metropoilis_Algorithm.cpp
@@ -8,6 +8,10 @@
 #include <iostream>
 #include <tuple>
 #include <random>
+#include <fstream>
+#include <ctime>
+#include <vector>
+
 using namespace std;
 
 #define L 100
@@ -16,21 +20,21 @@ using namespace std;
 #define YNN L
 #define B 0
 #define J 1
-#define bin 25
+#define bin 40
 
 static int s[N];
 static double prob[5]; // 1 1 exp 1 exp
-// double beta = 1;
 static random_device rd;  // Will be used to obtain a seed for the random number engine
 static mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
 static uniform_real_distribution<> dis(0.0, 1.0);
+static int Fliped_Step;
+static int Total_Step;
 
-void prob_calc(double b){
+void prob_calc(double beta){
     int i;
     for(i = 2; i < 5; i += 2){
-        prob[i] = exp(-2*b*i);
+        prob[i] = exp(-2*beta*i);
     }
-    // cout << prob[2] << " " << prob[4] << "\n";
 }
 
 void initialize(double beta){
@@ -42,15 +46,15 @@ void initialize(double beta){
 }
 
 int sweep_helical(int i){
-    int nn, sum;
+    int nn, sum = 0;
 
-    if((nn = i + XNN) >= N) nn -= N;
-    sum = s[nn];
     if((nn = i - XNN) < 0) nn += N;
+    sum += s[nn];
+    if((nn = i + XNN) >= N) nn -= N;
+    sum += s[nn];
+    if((nn = i - YNN) < 0) nn += N;
     sum += s[nn];
     if((nn = i + YNN) >= N) nn -= N;
-    sum += s[nn];
-    if((nn = i - XNN) < 0) nn += N;
     sum += s[nn];
 
     return sum;
@@ -58,16 +62,17 @@ int sweep_helical(int i){
 
 int helical(int i){
     int nn, sum;
-    if((nn = i + XNN) >= N) nn -= N;
+    
+    if((nn = i + XNN) == N) nn = 0;
     sum = s[nn];
+
     if((nn = i + YNN) >= N) nn -= N;
     sum += s[nn];
 
     return sum;
 }
 
-
-tuple<int,int> measure(){
+vector<int> measure(){
     int i, sum, HH;
     int res = 0, sigma = 0;
     for(i = 0; i < N; i++){
@@ -78,69 +83,83 @@ tuple<int,int> measure(){
     
     HH = -res-B*sigma;
 
-    return tuple<int,int>(HH,sigma);
+    return vector<int>({HH,sigma});
 }
 
 int calculate(int n = N){
     int i, k, delta;
+    double a;
     for(i = 0; i < n; i++){
-        k = int(N*dis(gen));
+        k = N*dis(gen);
+
+        delta = sweep_helical(k)*s[k];
         
-        delta = 2*J*sweep_helical(k)*s[k];
-        // cout << delta << " "  <<  k <<  " "<< sweep_helical(k) << " " << s[k] << "\n";
-        
+        a = dis(gen);
+        Total_Step++;
+
         if(delta <= 0){
-            s[k] *= -1;
-        } else if(dis(gen) < prob[int(delta/2)]){
-            // cout << "fliped" << "\n";
-            s[k] *= -1;
+            Fliped_Step++;
+            s[k] = -s[k];
+        } else if(a < prob[delta]){
+            Fliped_Step++;
+            s[k] = -s[k];
         }
     }
-    // cout << k << " " << delta << " " << s[k] << "\n";
 }
 
 int main(){
-   
-    // for(int i = 0; i < 10; i++){
-    //     cout << int(10*dis(gen)) << " ";
-    // }
-
-    tuple<int,int> value;
-    double m[25];
-    double c[25];
-    double res[4];
+    vector<int> value;
+    vector<double> m(bin);
+    vector<double> c(bin);
+    vector<vector<double>> res(bin,vector<double>(4,0));
 
     int sigma, HH;
 
-    for(int i = 0; i < 25; i++){
-        for(int j = 0; j < 4; j++){
-            res[j] = 0;
-        }
-        double beta = 1/(0.2*(i+1));
+    for(int i = 0; i < bin; i++){
+        Fliped_Step = 0;
+        Total_Step = 0;
+        double T = (5/double(bin))*(i+1);
+        double beta = 1/T;
 
         initialize(beta);
         for(int j = 0; j < 2000; j++){
             calculate();
         }
         value = measure();
-        sigma = get<1>(value);
-        HH = get<0>(value);
-        cout << sigma << " " <<HH << "\n";
+        HH = value[0];
+        sigma = value[1];
+
+        cout <<"idx: " << i << "||" << sigma << " " << HH << "\n";
 
         int epoch = 18000;
         for(int j = 0; j < epoch; j++){
             calculate();
+            
             value = measure();
-            sigma = get<1>(value);
-            HH = get<0>(value);
-
-            res[0] += abs(sigma)/double(epoch);
-            res[1] += sigma*sigma/double(epoch);
-            res[2] += HH/double(epoch);
-            res[3] += HH*HH/double(epoch);
+            HH = value[0];
+            sigma = value[1];
+           
+            res[i][0] += abs(sigma)/double(epoch);
+            res[i][1] += (sigma*sigma)/double(epoch);
+            res[i][2] += HH/double(epoch);
+            res[i][3] += (HH*HH)/double(epoch);
         }
-        m[i] = res[0]/N;
-        c[i] = beta*beta*(res[3]-res[2]*res[2])/N;
-        cout << m[i] << " " << c[i] << "\n";
-    }    
-} 
+        m[i] = res[i][0]/N;
+        c[i] = (beta*beta)/N*(res[i][3]-(res[i][2]*res[i][2]));
+        cout << m[i] << " " << c[i] << " " << Fliped_Step << " " << Total_Step << '\n';
+    }
+    
+    bool save = true;
+
+    if (save){
+        ofstream myfile;
+        myfile.open("Metropolis_c_"+to_string(L)+".csv");
+        myfile << ",temperture,magnetization,specific heat,abs(sigma),sigma**2,HH,HH**2\n";
+        for(int i = 0; i <bin; i++){
+            string temp = to_string(i) + "," + to_string((5/double(bin))*(i+1)) + "," + to_string(m[i]) + "," + to_string(c[i]) + ",";
+            temp = temp + to_string(res[i][0]) + "," + to_string(res[i][1]) + "," + to_string(res[i][2]) + "," + to_string(res[i][3]) + "\n";
+            myfile << temp;
+        }
+        myfile.close();
+    }
+}
